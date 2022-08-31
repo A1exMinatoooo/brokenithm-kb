c9e86bac144230815609e33ed6aae6cb4b0bdb9f
A1exMinatoooo <21982749+A1exMinatoooo@users.noreply.github.com>
2022-09-01 04:20:42 +0900

    Update KeyboardSimulator.cpp

--- src/src/KeyboardSimulator.cpp
+++ src/src/KeyboardSimulator.cpp
@@ -14,13 +14,21 @@ WORD TASOLLER_BTN_MAP[N_BUTTONS] = {};
 // bottom row sensors:          642zxvtrpnljhfdb
 // air sensors (down to up):    -=[]\;
 
+// WORD YUANCON_BTN_MAP[N_BUTTONS] = {
+//     '6', '5', '4', '3', '2', '1', 'Z', 'Y',
+//     'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q',
+//     'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I',
+//     'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A',
+//     VK_OEM_MINUS, VK_OEM_PLUS, VK_OEM_4,
+//     VK_OEM_6, VK_OEM_5, VK_OEM_1};
+
 WORD YUANCON_BTN_MAP[N_BUTTONS] = {
-    '6', '5', '4', '3', '2', '1', 'Z', 'Y',
-    'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q',
-    'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I',
-    'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A',
-    VK_OEM_MINUS, VK_OEM_PLUS, VK_OEM_4,
-    VK_OEM_6, VK_OEM_5, VK_OEM_1};
+    'S', 'S', 'D', 'D', 'F', 'F', 'G', 'G',
+    'H', 'H', 'J', 'J', 'K', 'K', 'L', 'L',
+    'S', 'S', 'D', 'D', 'F', 'F', 'G', 'G',
+    'H', 'H', 'J', 'J', 'K', 'K', 'L', 'L',
+    VK_SPACE, VK_SPACE, VK_SPACE,
+    VK_SPACE, VK_SPACE, VK_SPACE};
 
 struct KeyboardSimulator::Impl
 {
